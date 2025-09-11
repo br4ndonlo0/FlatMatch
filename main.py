91b6666929b42b7b82fbd0a3905005640bdf8809
Jordan <giorhojoestar@gmail.com>
2025-09-11 17:27:00 +0800

    structure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-print("Hello, World!")
-print ("Hello there")
-print("Hello world")
-print("hellooooo")
-print("fuckyou")
+from app import create_app
 
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(debug=True)
